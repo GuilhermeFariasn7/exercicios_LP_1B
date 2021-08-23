1faa0ecdb3da2afdd716cf02ac50f20ce21640b6
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-08-23 13:58:05 -0300

    Create Exer 3.py

---
+++ Exer 3.py
@@ -0,0 +1,9 @@
+""" Exer 3 """
+
+mes = input("Digite o mês do seu nascimento: ")
+
+ano = int(input("Digite o ano do seu nascimento: "))
+
+trab = input("Trabaha em que cargo?")
+
+print("{},nascido no mês de {} e no ano de {}, trabalha de {}. data formada: {}/{}".format(nome,mes,ano,trab,mes,ano))
