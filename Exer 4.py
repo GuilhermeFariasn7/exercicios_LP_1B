21ed1489da22393367a03554e7e614725642b09b
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-08-23 15:46:55 -0300

    Create Exer 4.py

---
+++ Exer 4.py
@@ -0,0 +1,8 @@
+write = input("digite seu nome: ")
+print("Olá "+write+" vou fazer uma soma para vc,")
+
+num1 = int(input("Digite o primeiro valor: "))
+num2 = int(input("Digite o segundo valor: "))
+s = num1+num2
+print("A soma do número {} mais o número {} é: {}".format(num1,num2,s))
+print(type(s))
